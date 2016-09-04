ed0820aefa0475b2ddaf499942d5224a879c58e5
Anh Mai <anh.mai@efe.com.vn>
2016-09-04 11:27:36 +0700

    Test multi languages

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,10 +7,12 @@
 namespace App\Controller;
 use App\Lib\Api;
 use Cake\Core\Configure;
+use Cake\I18n\I18n;
 
 class DashboardController extends AppController {
     
     public function index() {
+        I18n::locale('de');
         $this->set('pageHeader', 'Dashboard');
     }
     
